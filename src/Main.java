a7297a9e0e6fe400c05e30667c36027f57bff56c
Sami Zreik <Samizr>
2023-01-09 17:59:39 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Read lines in a loop
+        // Run lexical / regex interpreter on line
+        // Run backend on that line
+        System.out.println("Please type calculator operations:");
+        BufferedReader inputReader =  new BufferedReader(new InputStreamReader(System.in));
+        inputReader.lines().forEachOrdered(System.out::println);   //TODO: Do we need the ordered? can reduce performance.
+            // Calculator.render line
+                // Calculator has interpreter section and backend section
+}}
+
+// Test cases:
+    // Whatever in doc
+    // Case of double enters then another line.
+
+// Future considerations:
+    // consider asynchronous operations on lines. But probably bad because sequantial functions matter.
